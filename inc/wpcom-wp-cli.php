f128e67a8abb680fea7955a869a1b51f401edcde
TommusRhodus <tom.rhodes@automattic.com>
2025-01-17 11:35:17 +0000

    Added a WPCom specific version

--- inc/wpcom-wp-cli.php
+++ inc/wpcom-wp-cli.php
@@ -1,9 +1,7 @@
 <?php
-/**
- * The Tumblr JSON importer WP-CLI command.
- *
- * @package Tumblr-JSON-Importer
- */
+declare( strict_types = 1 );
+
+namespace CupcakeLabs\API;
 
 /**
  * Run the Tumblr JSON importer.
@@ -38,6 +36,13 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 		)
 	);
 
+	// Ensure this can only run on https://cupcakelabsdemo.wordpress.com/
+	$blog_id = wpcom_get_blog_id();
+
+	if ( 240546002 !== $blog_id ) {
+		\WP_CLI::error( 'Not on Cupcake Labs testing blog, add the following to your argument: --url=https://cupcakelabsdemo.wordpress.com/' );
+	}
+
 	// Ensure that the file exists.
 	$file_path = $args[0];
 
@@ -50,7 +55,7 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 
 	// Check if the file exists.
 	if ( ! $wp_filesystem->exists( $file_path ) ) {
-		WP_CLI::error( "The file '{$file_path}' does not exist." );
+		\WP_CLI::error( "The file '{$file_path}' does not exist." );
 	}
 
 	// Read the file contents using the WordPress Filesystem API.
@@ -58,7 +63,7 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 
 	// Check if the file could be read.
 	if ( false === $json_data ) {
-		WP_CLI::error( "Failed to read the file '{$file_path}'." );
+		\WP_CLI::error( "Failed to read the file '{$file_path}'." );
 	}
 
 	// Decode the JSON data.
@@ -66,11 +71,11 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 
 	// Check if the JSON is valid.
 	if ( json_last_error() !== JSON_ERROR_NONE ) {
-		WP_CLI::error( 'Invalid JSON file: ' . json_last_error_msg() );
+		\WP_CLI::error( 'Invalid JSON file: ' . json_last_error_msg() );
 	}
 
 	// Tell the user how many posts were found.
-	WP_CLI::log(
+	\WP_CLI::log(
 		sprintf(
 			'JSON file read successfully. Found %d posts.',
 			count( $json_posts )
@@ -78,7 +83,7 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 	);
 
 	// Args verified, let's start the import.
-	WP_CLI::log( 'Starting Tumblr JSON Importer...' );
+	\WP_CLI::log( 'Starting Tumblr JSON Importer...' );
 
 	// Ensure that WordPress is in the importing state.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
@@ -127,8 +132,9 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 	}
 
 	if ( 'true' === $assoc_args['dry-run'] ) {
-		WP_CLI::debug( print_r( $posts_data, true ) );
-		WP_CLI::success( 'Dry run complete. No posts were imported. Set --debug to see the generated posts array.' );
+		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r -- This command is for dev purposes only.
+		\WP_CLI::debug( print_r( $posts_data, true ) );
+		\WP_CLI::success( 'Dry run complete. No posts were imported. Set --debug to see the generated posts array.' );
 		exit;
 	}
 
@@ -140,11 +146,11 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 		$post_id     = wp_insert_post( $post_data, true, false );
 		$tumblr_data = maybe_unserialize( $post_data['meta_input']['_tumblr_data'] );
 
-		WP_CLI::log( 'Importing Tumblr post: ' . $tumblr_data['id'] );
+		\WP_CLI::log( 'Importing Tumblr post: ' . $tumblr_data['id'] );
 
 		// Attempt to gracefully handle errors.
 		if ( is_wp_error( $post_id ) ) {
-			WP_CLI::error(
+			\WP_CLI::error(
 				sprintf(
 					'Failed to import tumblr post: %s. Imported %d posts before this. Error: %s',
 					$tumblr_data['id'],
@@ -154,15 +160,61 @@ function tumblr_json_importer_run( $args, $assoc_args ): void {
 			);
 		}
 
-		WP_CLI::log( 'Imported Tumblr post: ' . $tumblr_data['id'] );
+		\WP_CLI::log( 'Imported Tumblr post: ' . $tumblr_data['id'] );
 
 		// Add the post ID to the imported array.
 		$imported[] = $tumblr_data['id'];
 	}
 
-	WP_CLI::debug( 'Imported Tumblr post IDs:' );
-	WP_CLI::debug( print_r( $imported, true ) );
-	WP_CLI::success( 'Import complete. Imported ' . count( $imported ) . ' posts.' );
+	\WP_CLI::debug( 'Imported Tumblr post IDs:' );
+
+	// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r -- This command is for dev purposes only.
+	\WP_CLI::debug( print_r( $imported, true ) );
+	\WP_CLI::success( 'Import complete. Imported ' . count( $imported ) . ' posts.' );
 }
 
-WP_CLI::add_command( 'tumblr-json-import', 'tumblr_json_importer_run' );
+if ( defined( '\WP_CLI' ) && \WP_CLI ) {
+	\WP_CLI::add_command( 'tumblr-json-import', __NAMESPACE__ . '\tumblr_json_importer_run' );
+}
+
+/**
+ * Add post_content_filtered to REST API responses.
+ *
+ * @todo This needs auth.
+ *
+ * @return void
+ */
+function posts_api_extensions(): void {
+	/**
+	 * Only run the following code for https://cupcakelabsdemo.wordpress.com/ or REST Requests
+	 * that call sites/240546002 in the request.
+	 */
+	if ( empty( $GLOBALS['wp']->query_vars['rest_route'] ) || ! strpos( $GLOBALS['wp']->query_vars['rest_route'], 'sites/240546002' ) ) {
+		return;
+	}
+
+	register_meta(
+		'post',
+		'_tumblr_data',
+		array(
+			'type'         => 'string',
+			'single'       => true,
+			'show_in_rest' => true,
+		)
+	);
+
+	register_rest_field(
+		'post',
+		'content_filtered',
+		array(
+			'get_callback' => function ( $post_data ) {
+				return get_post_field( 'post_content_filtered', $post_data['id'] );
+			},
+			'schema'       => array(
+				'description' => __( 'The post content filtered.', 'tumblr-json-importer' ),
+				'type'        => 'string',
+			),
+		)
+	);
+}
+add_action( 'rest_api_init', __NAMESPACE__ . '\posts_api_extensions' );
